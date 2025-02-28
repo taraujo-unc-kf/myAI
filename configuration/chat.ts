965590b2fb800cceac9583d052d31f0e6a328284
taraujo-unc-kf <araujo.b.thiago@gmail.com>
2025-02-28 12:42:17 -0500

    Update chat.ts
    Chat 1

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,7 +1,7 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm Lloyd, ${OWNER_NAME}'s AI assistant.`;
-export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
+export const INITIAL_MESSAGE: string = `Hello, I'm Lloyd, Neo's AI assistant.`;
+export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Go play with something else.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
-export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
-export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
+export const WORD_BREAK_MESSAGE: string = `Go take a hike or just give me a minute`;
+export const HISTORY_CONTEXT_LENGTH: number = 20; // Number of messages to use for context when generating a response
