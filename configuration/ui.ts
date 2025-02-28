84937bf95eab687fad1d99730a53939cb6e1217d
taraujo-unc-kf <araujo.b.thiago@gmail.com>
2025-02-28 12:46:28 -0500

    Update ui.ts
    ui 1.0

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `AI Lloyd`;
+export const CHAT_HEADER: string = `AI Lloyd here to help`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
+export const FOOTER_MESSAGE: string = `Lloyd the AI`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${Lloyd}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_TITLE: string = `Lloyd the AI`;
+export const PAGE_DESCRIPTION: string = `Chat with Lloyd, Neo's AI assistant.`;
 
-export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
+export const EMPTY_CITATION_MESSAGE: string = "Who dis?";
