0f93bf7555946364e99f4c0f254917076d133caf
taraujo-unc-kf <araujo.b.thiago@gmail.com>
2025-02-28 13:58:16 -0500

    Update chat.ts
    chat word break update

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -3,5 +3,5 @@ import { OWNER_NAME, AI_NAME } from "./identity";
 export const INITIAL_MESSAGE: string = `Hello, I'm Lloyd, Neo's AI assistant.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Go play with something else.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
-export const WORD_BREAK_MESSAGE: string = `Go take a hike or just give me a minute`;
+export const WORD_BREAK_MESSAGE: string = `Go take a hike or just give me a minute, your call.`;
 export const HISTORY_CONTEXT_LENGTH: number = 20; // Number of messages to use for context when generating a response
